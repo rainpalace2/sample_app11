81f7f5ccd585e636686997f2aa0b5f9aa3a5ef66
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-08-08 04:57:45 +0000

    [Finish]sample_app7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,10 +18,25 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
-  private
-  def list_params
-    params.require(:list).permit(:title,:body)
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+  
+  private
+
+  def list_params
+    params.require(:list).permit(:title,:body,:image)
+  end
+
 end
